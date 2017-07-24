2ecbc90c363de76b0fa50b3a74676e91daf40f81
Eugene DiBenedetto <eldibenedetto@gmail.com>
2017-07-24 17:40:26 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length > 2
+    last_element = array.pop
+    new_string = array.join ", "
+    new_string << ", and #{last_element}"
+  else
+    array.join " and "
+  end
 end
